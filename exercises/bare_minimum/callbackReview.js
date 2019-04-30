e0c23c6137fd9474eb624d84edb5ec30465a349b
matt-violet <mattviolet@gmail.com>
2019-04-30 15:24:36 -0700

    Implement callBackReview.js

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,40 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  // create a promise with the new Promise constructor
+  const getFirstLinePromise = new Promise((resolve, reject) => {
+    // do something async, then 
+    fs.readFile(filePath, function (err, fileData) {
+      if (err) {
+        reject(err);
+        callback(err);
+      } else {
+        var firstLine = fileData.toString().substring(0, fileData.indexOf('\n'));
+        resolve(firstLine);
+        callback(err, firstLine);
+      }
+    });
+  });
+
+  return getFirstLinePromise;
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  const statusCodePromise = new Promise((resolve, reject) => {
+    request(url, function(err, res, body) {
+      if (err) {
+        reject(err);
+        callback(err);
+      } else {
+        resolve(res.statusCode);
+        callback(err, res.statusCode);
+      }
+    });
+  });
+
+  return statusCodePromise;
 };
 
 // Export these functions so we can test them and reuse them in later exercises
